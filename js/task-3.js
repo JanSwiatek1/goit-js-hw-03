6d5fa80cd21b31cf87fe5accd16c9d27c3495b6b
Jan Świątek <swiatek-jan@wp.pl>
2025-02-28 18:05:51 +0100

    task-3 completed

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    let numberArray = [];
+    for (const number of numbers) {
+        if (number>value) {
+            numberArray.push(number);
+        }
+    }
+    return numberArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
